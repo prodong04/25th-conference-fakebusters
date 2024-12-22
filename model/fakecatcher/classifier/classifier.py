ede377176c92c0a543a840d9de973e59300ffa4e
yeon-ee <2019147559@yonsei.ac.kr>
2024-12-22 15:06:24 +0000

    refactor: split ppg.py, cnn.py

---
+++ model/fakecatcher/classifier/classifier.py
@@ -0,0 +1,16 @@
+from basecnn import baseCNN
+from resnet import ResNet34
+from effinet import EfficientNetB3
+
+
+class Model:
+    @staticmethod
+    def get_model(model_name):
+        if model_name == 'baseCNN':
+            return baseCNN()
+        elif model_name == 'ResNet34':
+            return ResNet34()
+        elif model_name == 'EfficientNetB3':
+            return EfficientNetB3()
+        else:
+            raise ValueError(f"Unknown model name: {model_name}")
