1c1ec28576562615bec42fec78401672f3b39c4d
Chaerim <76841211+C-limlim@users.noreply.github.com>
2024-12-23 21:46:17 +0900

    feat: add fake api for model server (#54)

--- backend/dbust-backend-fastapi/routers/simulate_model_sever.py
+++ backend/dbust-backend-fastapi/routers/simulate_model_sever.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, File, UploadFile
 from fastapi.responses import StreamingResponse
 import random 
 import httpx
+import os, shutil
 
 router = APIRouter(
     prefix="/api/models",
@@ -9,26 +10,29 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/lip/{filekey}")
-async def roi_model(filekey: str):
+VIDEO_DIR = "data/video"
+
+@router.post("/lipforensic")
+async def roi_model(file: UploadFile = File(...)):
     '''
     Simulate the lipforensic model server
     Input: filekey: str
     Output: response: StreamingResponse which contains the video file and score in the header
     '''
-    # todo: add logic to get the video file from lipforensic model server
-    video_file_path = "lip_sample.mp4"
-
-    video_file = open(video_file_path, "rb")
+    
+    file_path = os.path.join(VIDEO_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+    video_file = open(file_path, "rb")
     score = random.randint(0, 100)
 
     response = StreamingResponse(video_file, media_type="video/mp4")
     response.headers['Score'] = f"{score}"
-    response.headers['FileKey'] = filekey
     return response
 
-@router.get("/mmnet/{filekey}")
-async def model(filekey: str):
+@router.post("/mmnet")
+async def model(file: UploadFile = File(...)):
     '''
     get response from mmnet model server
     Input: filekey: str
@@ -36,27 +40,41 @@ async def model(filekey: str):
     '''
 
     model_sever_url = "http://165.132.46.87:30310/process_video/"
-    video_file_path = "lip_sample.mp4"
-    # headers = {
-    #     "Content-Type": "multipart/form-data"
-    # }
+    
+    file_path = os.path.join(VIDEO_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+    video_file = open(file_path, "rb")
+    score = random.randint(0, 100)
+    
+    response = StreamingResponse(video_file, media_type="video/mp4")
+    response.headers['Score'] = f"{score}"
+    return response
 
-    try: 
-        with open(video_file_path, "rb") as video_file:
-            async with httpx.AsyncClient() as client:
-                response = await client.post(model_sever_url, files={"file": video_file})
-                response.raise_for_status()
-                return response.json()
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(model_sever_url, files={"file": video_file})
+            response.raise_for_status()
+            return response.json()
     except httpx.HTTPStatusError as e:
         raise HTTPException(status_code=e.response.status_code, detail=f"Error from model server: {e.response.text}")
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
     
-@router.get("/faceroi/{filekey}")
-async def face_roi_model(filekey: str):
+@router.post("/faceroi")
+async def face_roi_model(file: UploadFile = File(...)):
     '''
     Simulate the Face ROI server
     Input: filekey: str
     Output: response: StreamingResponse which contains the video file and score in the header
     '''
+    file_path = os.path.join(VIDEO_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+    video_file = open(file_path, "rb")
+    score = random.randint(0, 100)
+    
+    response = StreamingResponse(video_file, media_type="video/mp4")
