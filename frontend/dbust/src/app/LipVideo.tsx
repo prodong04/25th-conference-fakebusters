575f524160a6173926ef7ebff214a2e925353e8e
sunghj1118 <sunghj1118@gmail.com>
2024-12-24 04:29:35 +0900

    feat: add LipVideo component

---
+++ frontend/dbust/src/app/LipVideo.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface AugmentedVideoProps {
+  videoUrl: string;
+  score: string | null;
+}
+
+const AugmentedVideo: React.FC<AugmentedVideoProps> = ({ videoUrl, score }) => {
+  return (
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto py-8 sm:py-12 lg:py-4">
+          <h1 className="text-3xl font-bold mb-4">Lip-Reading ROI Video</h1>
+
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-1 lg:gap-x-6 lg:space-y-0">
+            <div className="group relative">
+              <video
+                className="w-full max-w-2xl rounded-lg bg-white object-cover group-hover:opacity-75"
+                autoPlay
+                loop
+                muted
+                src={videoUrl}
+                style={{ aspectRatio: '16/9' }} // Adjust aspect ratio to match original dimensions
+              />
+              <h3 className="mt-6 text-sm text-gray-500">Lip-Reading ROI Video Score: {score}</h3>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AugmentedVideo;
