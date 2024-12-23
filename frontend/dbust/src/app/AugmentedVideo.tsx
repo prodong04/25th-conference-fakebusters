92747bd860441b14a448e57fad71a34b1fb9859e
sunghj1118 <sunghj1118@gmail.com>
2024-12-24 05:05:50 +0900

    feat: round loss to two decimals

--- frontend/dbust/src/app/AugmentedVideo.tsx
+++ frontend/dbust/src/app/AugmentedVideo.tsx
@@ -6,11 +6,14 @@ interface AugmentedVideoProps {
 }
 
 const AugmentedVideo: React.FC<AugmentedVideoProps> = ({ videoUrl, score }) => {
+  const formattedScore = score ? parseFloat(score).toFixed(2) : null;
+
+
   return (
     <div className="bg-gray-100">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto py-8 sm:py-12 lg:py-4">
-          <h1 className="text-3xl font-bold mb-4">Augmented Video</h1>
+          <h1 className="text-3xl font-bold mb-4">Lip-Reading ROI Video</h1>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-1 lg:gap-x-6 lg:space-y-0">
             <div className="group relative">
@@ -22,7 +25,7 @@ const AugmentedVideo: React.FC<AugmentedVideoProps> = ({ videoUrl, score }) => {
                 src={videoUrl}
                 style={{ aspectRatio: '16/9' }} // Adjust aspect ratio to match original dimensions
               />
-              <h3 className="mt-6 text-sm text-gray-500">Augmented Video Score: {score}</h3> {/* Update this line */}
+              <h3 className="mt-6 text-sm text-gray-500">Augmented Video Loss: {formattedScore}</h3>
               </div>
           </div>
         </div>
